b93eb9a880f21b05734cb40e899bd0d72fc7e1a5
Alexander Wagner <ecodat@posteo.de>
2026-02-04 14:56:34 +0100

    refined processing script and created a new function to process deployments (optionally with temporal species filter based on week of audio file) updated manual

---
+++ R/fct_process_deployment.R
@@ -0,0 +1,190 @@
+#' Process a single deployment with BirdNET inference
+#'
+#' Main entry point for processing a deployment:
+#' - filters audio files
+#' - sets up BirdNET model
+#' - generates settings
+#' - applies inference
+#' - aggregates results
+#'
+#' @param deployment_id Integer. ID of the deployment to process.
+#' @param deployments Data frame / sf object. Deployment metadata.
+#' @param audio_files Data frame. Audio files metadata.
+#' @param temporal_filtering Logical. Whether to group audio files by week to facilitate temporal species filtering.
+#' @param occurence_min_confidence Numeric. Minimum eBird occurrence confidence. If 0, all available species in Birdnet will be used
+#' @param birdnet_params_list List. Optional parameters to override BirdNET defaults.
+#' @param verbose Logical. Print progress messages.
+#' @param internal_pool DBI pool object. If NULL, a new pool will be created.
+#'
+#' @return Data frame of BirdNET inference results.
+#' @export
+process_deployment_birdnet <- function(deployment_id,
+                               deployments,
+                               audio_files,
+                               temporal_filtering,
+                               occurence_min_confidence,
+                               birdnet_params_list = list(),
+                               verbose = TRUE,
+                               internal_pool = NULL) {
+  if(is.null(internal_pool)) internal_pool <- set_db_pool()
+  on.exit(pool::poolClose(internal_pool), add = TRUE)
+
+  if(verbose) message("Processing deployment: ", deployment_id)
+
+  dep_info <- get_deployment_info(deployments, deployment_id)
+  audio_files_subset <- get_audio_files_for_deployment(audio_files, deployment_id, temporal_filtering)
+
+  birdnet_inference_list_weekly <- vector("list", length = unique(audio_files_subset$week))
+
+  for(week_i in seq_along(unique(audio_files_subset$week))) {
+    week <- unique(audio_files_subset$week)[week_i]
+    if(week == -999) week <- NULL
+
+    model_info <- prepare_birdnet_model(dep_info$coordinates[2],
+                                        dep_info$coordinates[1],
+                                        week,
+                                        occurence_min_confidence,
+                                        birdnet_params_list)
+    bnm <- model_info$bnm
+    birdnet_params <- model_info$birdnet_params
+
+    settings_id <- upsert_birdnet_settings(internal_pool, bnm, birdnet_params)
+    possible_species <- get_possible_species(bnm$species, species)
+
+    birdnet_inference_list_weekly[[week_i]] <- run_birdnet_inference(audio_files_subset,
+                                                                     bnm,
+                                                                     birdnet_params,
+                                                                     settings_id,
+                                                                     possible_species)
+  }
+
+  return(
+    dplyr::bind_rows(birdnet_inference_list_weekly)
+  )
+
+}
+
+# ------------------ Internal helper functions ------------------
+
+#' @keywords internal
+get_deployment_info <- function(deployments, deployment_id) {
+  deployment <- deployments |>
+    dplyr::filter(.data$deployment_id == !!deployment_id)
+
+  xy <- deployment |> sf::st_coordinates() |> apply(2, mean)
+
+  return(list(deployment = deployment, coordinates = xy))
+}
+
+#' @keywords internal
+get_audio_files_for_deployment <- function(audio_files, deployment_id, temporal_filtering) {
+  audio_files_subset <- audio_files |>
+    dplyr::filter(deployment_id == deployment_id)
+
+  if(temporal_filtering){
+    audio_files_subset <- audio_files_subset |>
+      dplyr::mutate(week = round(mean(lubridate::week(timestamp_start))*52/50))
+  } else {
+    audio_files_subset$week <- -999
+  }
+
+  return(audio_files_subset)
+}
+
+#' @keywords internal
+prepare_birdnet_model <- function(latitude, longitude, week, min_confidence, birdnet_params_list = list()) {
+  bnm <- setup_birdnet_model(version = "v2.4",
+                             latitude = latitude,
+                             longitude = longitude,
+                             week = week,
+                             min_confidence = min_confidence)
+
+  defaults <- list(
+    model_name = bnm$model_name,
+    model_version = bnm$model_version,
+    min_confidence = 0.2,
+    chunk_overlap_s = 0,
+    use_bandpass = TRUE,
+    bandpass_fmin = 150L,
+    bandpass_fmax = 15000L,
+    apply_sigmoid = TRUE,
+    sigmoid_sensitivity = 1,
+    keep_empty = FALSE,
+    locale = "de",
+    latitude = latitude,
+    longitude = longitude,
+    week = week,
+    ebird_min_confidence = min_confidence
+  )
+
+  birdnet_params <- modifyList(defaults, birdnet_params_list)
+  list(bnm = bnm, birdnet_params = birdnet_params)
+}
+
+#' @keywords internal
+upsert_birdnet_settings <- function(pool, bnm, birdnet_params) {
+  settings <- data.frame(
+    model_name = bnm$model_name,
+    model_version = bnm$model_version,
+    min_conf = birdnet_params$min_confidence,
+    overlap = birdnet_params$chunk_overlap_s,
+    locale = birdnet_params$locale,
+    model_params = I(list(birdnet_params))
+  )
+
+  settings$model_params <- lapply(settings$model_params, jsonlite::toJSON, auto_unbox = TRUE)
+
+  query <- "SELECT * FROM import.settings WHERE model_params @> $1::jsonb;"
+  existing <- dbGetQuery(pool, query, params = settings$model_params)
+
+  if(nrow(existing) == 0){
+    new_settings_id <- upsert_settings_df(pool, settings)
+    replace_settings_species(conn = pool, settings_id = new_settings_id, species_id = birdnet_params$species_ids)
+  } else {
+    new_settings_id <- existing$settings_id[1]
+  }
+  new_settings_id
+}
+
+#' @keywords internal
+get_possible_species <- function(bnm_species, species_lut) {
+  species_list <- data.frame(
+    species_scientific = (bnm_species$label |> stringr::str_split("_", simplify = TRUE, n = 2))[,1]
+  )
+  species_list |>
+    dplyr::left_join(species_lut) |>
+    dplyr::select(species_scientific, species_id)
+}
+
+#' @keywords internal
+run_birdnet_inference <- function(audio_files_subset, bnm, birdnet_params, settings_id, possible_species_df) {
+  inference_list <- vector("list", length = nrow(audio_files_subset))
+
+  for(i in seq_len(nrow(audio_files_subset))){
+    res <- apply_birdnet_model(
+      audio_file = audio_files_subset$full_path[i],
+      birdnet_setup = bnm,
+      birdnet_params = birdnet_params
+    )$prediction_raw
+
+    res <- res |>
+      dplyr::select(-common_name) |>
+      dplyr::mutate(
+        audio_file_id = audio_files_subset$audio_file_id[i],
+        begin_time_ms = as.integer(round(start * 1000)),
+        end_time_ms   = as.integer(round(end * 1000)),
+        settings_id   = settings_id
+      ) |>
+      dplyr::rename(species_scientific = scientific_name) |>
+      dplyr::left_join(possible_species_df, by = "species_scientific") |>
+      dplyr::mutate(
+        behavior_id = NA_integer_,
+        confidence = as.integer(round(confidence * 10000))
+      ) |>
+      dplyr::select(audio_file_id, settings_id, begin_time_ms, end_time_ms, confidence, species_id, behavior_id)
+
+    inference_list[[i]] <- res
+  }
+
+  dplyr::bind_rows(inference_list)
+}
--- inst/local_file_process_script.R
+++ inst/local_file_process_script.R
@@ -1,118 +1,53 @@
-#establish connection
 
+
+###### define processing params
+project_id = 2
+occurence_min_confidence = 0.03 #e bird occurence confidence threshold for species filtering
+spatial_filtering = TRUE #will filter species list spatially (e bird occurence data) - if FALSE, occurence min confidence will be set to 0
+temporal_filtering = TRUE # will group audio_files by week (1 - 50) to generate time specific species list (will be reset to FALSE if spatial_filtering is FALSE)
+birdnet_params_list = list() #here, parameters can be specified (for instance changing default minimum confidence of inference results from 0.2 to 0.1), an empty list will result in default settings
+
+#####
+if(!spatial_filtering){
+  occurence_min_confidence <- 0
+  temporal_filtering <- FALSE
+}
+
+
+#establish connection
 pool <- set_db_pool()
 
 # get deployments, audio_files and species lut
-deployments <- sf::st_read(pool, DBI::Id("import", "deployments"))
-audio_files <- DBI::dbReadTable(pool, DBI::Id("import", "audio_files"))
+deployments <- sf::st_read(pool, DBI::Id("import", "deployments")) |>
+  dplyr::filter(.data$project_id == !!project_id)
+
+audio_files <- dplyr::tbl(pool, DBI::Id("import", "audio_files")) |>
+  dplyr::filter(deployment_id %in% deployments$deployment_id) |>
+  dplyr::collect()
+
 species <- DBI::dbReadTable(pool, DBI::Id("lut_species_code"))
 
 # join audio_files and deployments to generate full path to audio_file
 audio_files <- audio_files |>
-  dplyr::select(deployment_id, audio_file_id, relative_path) |>
+  dplyr::select(deployment_id, audio_file_id, relative_path,  timestamp_start) |>
   dplyr::left_join(deployments |>
                      dplyr::select(deployment_id, deployment_path) |>
                      sf::st_drop_geometry()) |>
   dplyr::mutate(full_path = paste0(deployment_path, relative_path))
 
-
-#parameterize birdnet
-
-# set long  / lat based on deployments (should be more fine-grained)
-xy <- deployments |>
-  sf::st_coordinates() |>
-  apply(2, mean)
-
-# set week based on deployments (should be more fine-grained)
-week = round(mean(lubridate::week(deployments$start_datetime))*52/50)
-
-# set occurence threshold (this will ultimately define the potential species)
-occurence_min_confidence = 0.03
-
-# initialize birdnet model
-bnm <- setup_birdnet_model(version = "v2.4", latitude = xy[2], longitude = xy[1], week = week, min_confidence = occurence_min_confidence)
-
-# full parameter list (will be stored as json blob in database)
-birdnet_params <- list(
-  model_name = bnm$model_name,
-  model_version = bnm$model_version,
-  min_confidence = 0.2,
-  chunk_overlap_s = 0,
-  use_bandpass = TRUE,
-  bandpass_fmin = 150L,
-  bandpass_fmax = 15000L,
-  apply_sigmoid = TRUE,
-  sigmoid_sensitivity = 1,
-  keep_empty = FALSE,
-  locale = "de",
-  latitude = xy[2],
-  longitude = xy[1],
-  week = week,
-  ebird_min_confidence = occurence_min_confidence
+# process deployments (use spatial and temporal information as species list filters as indicated before)
+results_list <- lapply(as.list(deployments$deployment_id),
+       process_deployment_birdnet,
+       deployments = deployments,
+       audio_files = audio_files,
+       temporal_filtering = temporal_filtering,
+       occurence_min_confidence = occurence_min_confidence,
+       birdnet_params_list = list()
 )
 
-# generate settings data frame compatible with database structure
-settings <- data.frame(
-  model_name = bnm$model_name,
-  model_version = bnm$model_version,
-  min_conf = birdnet_params$min_confidence,
-  overlap = birdnet_params$chunk_overlap_s,
-  locale = birdnet_params$locale,
-  model_params = I(list(birdnet_params))
-)
-
-# generate json object from parameter list
-settings$model_params <- lapply(settings$model_params, jsonlite::toJSON, auto_unbox = TRUE)
-
-# check if these settings are already in the database (otherwise there will be - harmless - redudancy) and either upsert or get existing settings id
-query <- "
-SELECT *
-FROM import.settings
-WHERE model_params @> $1::jsonb
-"
-
-setting_available <- dbGetQuery(pool, query, params = settings$model_params)
-
-if(nrow(setting_available) == 0){
-  new_settings_id <- upsert_settings_df(pool, settings)
-
-  # if it is a new setting, also update species-settings junction table (this will delete previous entries for this settings id)
-  species_list <- data.frame(species_scientific = (bnm$species$label |>
-                                                     stringr::str_split("_", simplify = TRUE, n = 2))[,1])
-
-  possible_species_df <- species_list |>
-    dplyr::left_join(species) |>
-    dplyr::select(species_scientific, species_id)
-
-  replace_settings_species(conn = pool, settings_id = new_settings_id, possible_species_df$species_id)
-
-}else{
-  new_settings_id <- setting_available$settings_id[i]
-}
-
-# generate birdnet inference
-birdnet_inference_list <- vector("list", length = nrow(audio_files))
-
-for(i in seq(nrow(audio_files))){
-  birdnet_inference_list[[i]] <- apply_birdnet_model(audio_files$full_path[i],
-                      birdnet_setup = bnm,
-                      use_arrow = FALSE,
-                      batch_size = 1L,
-                      birdnet_params = birdnet_params)$prediction_raw |>
-    dplyr::select(-common_name) |>
-    dplyr::mutate(audio_file_id = audio_files$audio_file_id[i]) |>
-    dplyr::mutate(begin_time_ms = as.integer(round(start * 1000))) |>
-    dplyr::mutate(end_time_ms = as.integer(round(end * 1000))) |>
-    dplyr::rename(species_scientific = scientific_name) |>
-    dplyr::mutate(settings_id = new_settings_id) |>
-    dplyr::left_join(possible_species_df) |>
-    dplyr::mutate(behavior_id = NA_integer_) |>
-    dplyr::mutate(confidence = as.integer(round(confidence * 10000))) |>
-    dplyr::select(audio_file_id, settings_id, begin_time_ms, end_time_ms, confidence, species_id, behavior_id)
-}
 
 # upload inference
-birdnet_inference <- birdnet_inference_list |>
+birdnet_inference <- results_list |>
   dplyr::bind_rows() |>
   upsert_results_df(conn = pool)
 
