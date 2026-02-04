1076abdcaf90a64d169c5f6c8aaef0b8e2d19427
Alexander Wagner <ecodat@posteo.de>
2026-02-04 12:23:36 +0100

    refined upload script for tables projects, deployments and audio_files

--- inst/local_file_upload_script.R
+++ inst/local_file_upload_script.R
@@ -1,12 +1,17 @@
 pool <- set_db_pool()
 
-project_id <- upsert_project(pool, "test", "test123", description = "123", contact = "23", organisation = "123")
+project_id <- upsert_project(pool,
+                             "test",
+                             "test123",
+                             description = "123",
+                             contact = "23",
+                             organisation = "123")
 
 out <- retrieve_local_file_info(project_id,
                                 project_folder = "~/Dokumente/sound_db/project_1/",
                                 folder_depth = 1)
 
-out$deployment_index |>
+deployment_ids <- out$deployment_index |>
   readr::read_csv() |>
   dplyr::mutate(geometry_x_4326 = runif(dplyr::n(), 0, 10)) |>
   dplyr::mutate(geometry_y_4326 = runif(dplyr::n(), 40, 50)) |>
@@ -15,9 +20,27 @@ out$deployment_index |>
   sf::st_set_crs(4326) |>
   upsert_deployments_sf(conn = pool)
 
-deployments <- DBI::dbReadTable(pool, DBI::Id("import", "deployments"))
+# check the uploaded data
+deployments <- dplyr::tbl(pool, DBI::Id("import", "deployments")) |>
+  dplyr::filter(.data$project_id == !!project_id &
+                  deployment_id %in% deployment_ids) |>
+  dplyr::collect()
 
-df <- out$audio_file_indices |>
+
+# if there are new deployments (aka audio_file_indices csvs) decide whether to upload
+# only this information or all available audio file data. this will not introduce
+# duplicates to the database as audio files are checked for duplicates by deployment_id
+# and timestamp but may take more time. if you are unsure whether all previous audio file
+# indices have been uploaded, just use out$all_audio_file_indices
+
+if(length(out$new_audio_file_indices) == 0){
+  audio_file_indices <- out$all_audio_file_indices
+}else{
+  audio_file_indices <- out$new_audio_file_indices
+}
+
+# convert audio file indices so it matches the
+df <- out$all_audio_file_indices |>
   lapply(readr::read_csv) |>
   dplyr::bind_rows() |>
   dplyr::rename(deployment_name = deployment_id) |>
@@ -25,7 +48,16 @@ df <- out$audio_file_indices |>
   dplyr::relocate(deployment_id, .before = "deployment_name") |>
   dplyr::select(-deployment_name)
 
-upsert_audio_files_df(conn = pool, df_audio = df)
+# upload audio file indices, retrieve ids of upload
+audio_file_ids <- upsert_audio_files_df(conn = pool,
+                                        df_audio = df,
+                                        update_if_exists = TRUE)
+
+# check the uploaded data (glimpse to avoid large download)
+audio_files <- dplyr::tbl(pool, DBI::Id("import", "audio_files")) |>
+  dplyr::filter(audio_file_id %in% audio_file_ids) |>
+  dplyr::glimpse() |>
+  dplyr::collect()
 
 pool::poolClose(pool)
 
